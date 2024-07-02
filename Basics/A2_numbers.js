139f1a059f3711056e34f748a8fdc39af1f1be19
Rubal <rahmed2210@gmail.com>
2024-07-02 11:49:46 -0400

    Strings

---
+++ Basics/A2_numbers.js
@@ -0,0 +1,19 @@
+let num = 5.5 + 2
+
+console.log(num)
+
+
+let num1 = 5.5 - -1
+console.log(num1)
+
+
+let num2 = 5.5 * 2
+console.log(num2)
+
+
+let num3 = 5.5 / 2
+console.log(num3)
+
+let x = 11
+let num4 = x + 1 * 2
+console.log(num4)
