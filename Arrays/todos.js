2371fed52856f5b1a4d5bfa23bdd0f5cf125c7af
Rubal <rahmed2210@gmail.com>
2024-07-16 17:14:43 -0400

    Arrays With Methods

--- Arrays/todos.js
+++ Arrays/todos.js
@@ -1,9 +1,21 @@
-// create an array with 5 todos
-//you have x todos print the first and second to last item --> todo walk the dog
-
 const todos = ['Order cat food', 'Clean kitchen', 'Buy food', 'Do work', 'Exercise']
+console.log(todos)
+
+//-----------------------------------------------------------------------------------------
+// Manipulating Arrays with Methods
+console.log('\r\n  Manipulating Arrays with Methods')
+//-----------------------------------------------------------------------------------------
+
+// Delete the 3rd item
+todos.splice(2, 1)
+
+// add a new item onto the end
+todos.push('Buy coffee')
+
+// remove the first item from the list
+todos.shift()
+
+
 
 console.log(`You have ${todos.length} todos!`)
-
-console.log(`Todo: ${todos[0]}`)
-console.log(`Todo: ${todos[todos.length - 2]}`)
+console.log(todos)
