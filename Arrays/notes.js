239ab2e830731a6ecfb5c91efc0d779a2ca8a5d1
Rubal <rahmed2210@gmail.com>
2024-07-17 19:49:18 -0400

    Looping Over Arrays

--- Arrays/notes.js
+++ Arrays/notes.js
@@ -1,37 +1,35 @@
+//-----------------------------------------------------------------------------------------
+console.log('\r\n Looping Over Arrays')
+//-----------------------------------------------------------------------------------------
+
+
 const notes = ['Note 1', 'Note 2', 'Note 3', 'Note4']
 
-//-----------------------------------------------------------------------------------------
-// Manipulating Arrays with Methods
-console.log('\r\n Manipulating Arrays with Methods')
-//-----------------------------------------------------------------------------------------
 
+// console.log(notes.pop())  
 
+// notes.push('My new Note')  
 
-//   /* notes.pop()  // remove something from the end */
-// console.log('Remove from the end: ' + notes.pop())
+// console.log(notes.shift())
 
-// /* notes.push('My new Note') // Push adds something on to the end */
-// console.log('adding at the end: ' + notes.push('My new Note')) 
+// notes.unshift('My first Note')
 
+// console.log(notes.splice(1, 1, 'This is the new second item'))
 
-//  /* shift --> method removes the very first item from your array */
-// console.log('Remove first item: ' + notes.shift())
+notes[2] = 'This is now the new note 3'
 
-//    /* unshift --> method add at the very first beginning of array */
-// console.log(notes.unshift('My first Note'))
-
-
-//  /* Splice -- allow us to add elements to the middle of array or remove elements from anywhere */
-//   /* -->take two argument
-//        --> 1st argument to splice is where you're trying to take the action
-//         --> 2nd argument we provide is the number of things we're interested in removing */
-
-// console.log('Splice removing item: ' + notes.splice(1, 1))
-// console.log('Splice removing and adding new item: ' + notes.splice(1, 0, 'This is the new second item'))
-// console.log('Splice removing and adding new item: ' + notes.splice(1, 1, 'This is the new 2 second item'))
-
-// Swap out one item
-notes[2] = 'This note getting there'
+//-----------------------------
+console.log('\r\n') // black space
+//-----------------------------
+notes.forEach(function (item, index) {
+    console.log(index)
+    console.log(item)
+})
 
 console.log(notes.length)
-console.log(notes)
+console.log(notes + '\r\n')
+
+notes.forEach(function (note, index) {
+    const num = index + 1
+    console.log(`${num}. ${note}`)
+})
