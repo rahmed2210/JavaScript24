b75f746499ee551c17b30c24f5b695f9392059b8
Rubal <rahmed2210@gmail.com>
2024-08-07 20:08:07 -0400

    Searching Arrays Part I

--- Arrays/notes.js
+++ Arrays/notes.js
@@ -1,35 +1,42 @@
-//-----------------------------------------------------------------------------------------
-console.log('\r\n Looping Over Arrays')
-//-----------------------------------------------------------------------------------------
+console.log('\r\n')
+
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
 
 
-const notes = ['Note 1', 'Note 2', 'Note 3', 'Note4']
-
-
-// console.log(notes.pop())  
-
-// notes.push('My new Note')  
+// console.log(notes.pop())
+// notes.push('My new note')
 
 // console.log(notes.shift())
+// notes.unshift('My first note')
 
-// notes.unshift('My first Note')
+// notes.splice(1, 1, 'This is the new second item')
 
-// console.log(notes.splice(1, 1, 'This is the new second item'))
-
-notes[2] = 'This is now the new note 3'
+// notes[2] = 'This is now the new note 3'
 
 //-----------------------------
 console.log('\r\n') // black space
 //-----------------------------
-notes.forEach(function (item, index) {
-    console.log(index)
-    console.log(item)
-})
+// notes.forEach(function (item, index) {
+//     console.log(index)
+//     console.log(item)
+// })
+
 
 console.log(notes.length)
-console.log(notes + '\r\n')
+console.log(notes)
 
-notes.forEach(function (note, index) {
-    const num = index + 1
-    console.log(`${num}. ${note}`)
+
+console.log('\r\n Searching Arrays Part I')
+const index = notes.findIndex(function (note, index) {
+    return note.title === 'Habbits to work on'
 })
+console.log(index)
