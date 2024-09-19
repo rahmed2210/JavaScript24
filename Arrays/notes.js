d06c2b53e667807d657ce448db2a116193d679a7
Rubal <rahmed2210@gmail.com>
2024-09-18 22:23:21 -0400

    Sorting Arrays

--- Arrays/notes.js
+++ Arrays/notes.js
@@ -9,6 +9,21 @@ const notes = [{
     body: 'Get a new seat'
 }]
 
+// Sorting Arrays
+const sortNotes = function (notes) {
+    notes.sort(function (a, b) {
+        if (a.title.toLowerCase() < b.title.toLowerCase()) {
+            return -1
+        } else if (b.title.toLowerCase() < a.title.toLowerCase()) {
+            return 1
+        } else {
+            return 0
+        }
+    })
+}
+
+
+
 const findNote = function (notes, noteTitle) {
     return notes.find(function (note, index) {
         return note.title.toLowerCase() === noteTitle.toLowerCase()
@@ -25,16 +40,10 @@ const findNotes = function (notes, query) {
     })
 }
 
-console.log(findNotes(notes, 'eating'))
+// console.log(findNotes(notes, 'eating'))
 
+// const note = findNote(notes, 'some other office modification')
+// console.log(note)
 
-
-// const findNote = function (notes, noteTitle) {
-//     const index = notes.findIndex(function (note, index) {
-//         return note.title.toLowerCase() === noteTitle.toLowerCase()
-//     })
-//     return notes[index]
-// }
-
-const note = findNote(notes, 'some other office modification')
-console.log(note)
+sortNotes(notes)
+console.log(notes)
